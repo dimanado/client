ed11dbd4b73567daabed431d073653b1b24e0d7d
Dmitry BRANAVITSKI <dima_nado@mail.ru>
2015-06-23 13:26:38 +0300

    fix bag read file

--- echo.rb
+++ echo.rb
@@ -2,11 +2,11 @@ require 'rubygems'
 require 'eventmachine'
 require 'json'
 class EventM
-  def self.push_request
+  def self.push_request(x1,x2,x3)
     EM.run {
       require 'em-http'
       EventMachine.run {
-        http = EventMachine::HttpRequest.new('http://localhost:3000/index').post body: {x1: '2', x2: '4',str: '+'}
+        http = EventMachine::HttpRequest.new('http://localhost:3000/index').post body: {x1: x1, x2: x2,str:x3}
         http.errback { p 'Uh oh'; EM.stop }
         http.callback {
           p http.response
--- main.rb
+++ main.rb
@@ -1,10 +1,12 @@
 require_relative 'echo'
 require_relative 'work_file'
 
-
-s = Work_file.read
-s.each do |g|
-  x1, x2, x3 = g.chomp.split(' ')
-  puts("x1 = #{x1},x2 = #{x2},x3 = #{x3}")
+Work_file.read.each do |g|
+  (x1, x2, x3 = g.chomp.split(' '))
+  if(!x1.nil? || !x2.nil? || !x3.nil?)
+    EventM.push_request(x1.to_i,x2.to_i,x3)
+  else
+    p 'строка пуста'
+  end
 end
-EventM.push_request
+
